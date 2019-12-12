87632529ff89dc979d94d951ce4ef028f80264b2
Benjamin Jensen <Benjaminmargon@hotmail.com>
2019-12-12 10:41:02 +0100

    Movement fortsat

--- sketch.js
+++ sketch.js
@@ -1,36 +1,45 @@
-let img;
-let grader;
-let speed;
-let text1;
-grader = 0;
-speed = 0;
+let x = innerWidth/2;
+let y = innerHeight/2;
+let n = 0;
 
 function preload() {
   img = loadImage('Rød_Trekant.png');
 }
 
-
 function setup() {  
   imageMode(CENTER);
   createCanvas(innerWidth, innerHeight);
- 
+
   }
 
 function draw() {
   background(0);
 
-  grader = grader + speed;
+  if (keyIsDown(LEFT_ARROW)) {
+    n = n -0.05;
+  }
 
-  translate(innerWidth/2, innerHeight/2);
-  rotate(grader);
-  image(img, 0, 0, 600, 300);
+  if (keyIsDown(RIGHT_ARROW)) {
+    n = n + 0.05;
+  }
 
-}
+  if (keyIsDown(UP_ARROW)) {
+    y = y - 5;
+  }
 
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    speed = speed - 0.01;
-  } else if (keyCode === RIGHT_ARROW) {
-    speed = speed + 0.01;
-  } 
+  if (keyIsDown(DOWN_ARROW)) {
+    y = y + 5;
+  }
+
+  if (x > innerWidth + 10) {
+    x = 0;
+  }
+
+
+
+  translate(x, y);
+  rotate(n + PI / 2);
+  image(img, 0, 0, 150, 150);
+  
+
 }
