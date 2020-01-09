122289bafb91a3b148801711c6dc7cef9ccca793
Benjamin Jensen <Benjaminmargon@hotmail.com>
2020-01-09 09:05:27 +0100

    Ændring

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ function draw() {
   for (var i = 0; i < asteroids.length; i++) {
     asteroids[i].render();
   }
+  
   for (var i = 0; i < laser.length; i++) {
     laser[i].render();
     laser[i].update();
@@ -31,9 +32,8 @@ function keyReleased() {
 
 function keyPressed() {
   if (key == ' ') {
-    laser.push(new Laser(ship.pos));
-  }
-  if (keyCode == RIGHT_ARROW) {
+    lasers.push(new Laser(ship.pos));
+  } else if (keyCode == RIGHT_ARROW) {
     ship.setRotation(0.1);
   } else if (keyCode == LEFT_ARROW) {
     ship.setRotation(-0.1);
