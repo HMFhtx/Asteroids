ad4c9b813ab11fa8af2892ef88138acc5d8673a7
Benjamin Jensen <Benjaminmargon@hotmail.com>
2020-01-16 09:34:38 +0100

    astroider done

--- sketch.js
+++ sketch.js
@@ -5,8 +5,11 @@ let lasers = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ship = new Ship();
+  for (let i = 0; i < 10; i++) {
   asteroids.push(new Asteroid());
 }
+}
+
 
 function draw() {
   background(0);
@@ -17,6 +20,8 @@ function draw() {
 
   for (var i = 0; i < asteroids.length; i++) {
     asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
   }
 
   for (var i = 0; i < lasers.length; i++) {
